aad0ed5942bdb572eed048862f154ae426b90031
soywiz <soywiz@gmail.com>
2012-12-15 08:42:00 +0100

    - Fix TemplateProvider.js case

--- test.bat
+++ test.bat
@@ -12,4 +12,4 @@ SET TESTS=
 FOR %%X IN (test\*.js) DO CALL :add_test %%X
 
 REM echo %TESTS%
-mocha --ui exports --globals name --reporter spec %* %TESTS%
+call build_release.bat && mocha --ui exports --globals name --reporter spec %* %TESTS%

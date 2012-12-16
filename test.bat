b7aff29afcdad90e492a601ea88f82cdfa99e30c
soywiz <soywiz@gmail.com>
2012-12-16 11:45:04 +0100

    - completely moved tests to .set

--- test.bat
+++ test.bat
@@ -8,8 +8,8 @@ EXIT /B
 
 :start
 
-SET TESTS= 
+SET TESTS=
 FOR %%X IN (test\*.js) DO CALL :add_test %%X
 
 REM echo %TESTS%
-call build_release.bat && mocha --ui exports --globals name --reporter spec %* %TESTS%
+call build_release.bat && mocha --ui exports --globals name --reporter spec %TESTS% -g ".*%1.*"
--- test/templateparser.test.js
+++ test/templateparser.test.js
@@ -10,7 +10,7 @@ module.exports = {};
 
 function handleSet(name, data) {
 	var parts = data.split('===');
-	var test = { title: 'untitled: ' + name, input: {}, expected: '', templates: {}, eval: '' };
+	var test = { title: 'untitled: ' + name, input: {}, expected: '', templates: {}, eval: '', exception: false };
 	for (var n = 0; n < parts.length; n++) {
 		var part = parts[n].trim();
 		var token = /^([\w:]+)\s+(.*)$/m.exec(part);
@@ -23,6 +23,7 @@ function handleSet(name, data) {
 				case 'input': test.input = JSON.parse(value); break;
 				case 'output': test.expected = value; break;
 				case 'eval': test.eval = value; break;
+				case 'exception': test.exception = value; break;
 				default: {
 					var pp = key.split(':');
 					switch (pp[0]) {
@@ -71,173 +72,3 @@ function handleSets(path, name) {
 }
 
 handleSets(__dirname, 'sets');
-
-module.exports = {
-	'test simple if': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test', '{% if 0 %}0{% else %}1{% if 1 %}2{% endif%}{% endif %} ');
-		assert.equal(
-			templateParser.compileAndRenderToString('test'),
-			"12 "
-		);
-	},
-	'test simple if elseif else': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test', '{% if v == 0 %}0{% elseif v == 1 %}1{% elseif v == 2 %}2{% else %}else{% endif %}');
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { v : 2 }),
-			"2"
-		);
-	},
-	'test nested for': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test',
-			"{% for x in range(0, 3) %}{% for y in range(0, 3) %}{{ x * y }}{% endfor %},{% endfor %}" +
-		"");
-		//console.log(templateParser.getEvalCode('test').output);
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { v: 2 }),
-			"0000,0123,0246,0369,"
-		);
-	},
-	'test for key/values': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test',
-			"{% for key, value in data %}{{ key }}:{{ value }},{% endfor %}" +
-		"");
-		//console.log(templateParser.getEvalCode('test').output);
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { data: { a : 1, b : 2, c : 'c' } }),
-			"a:1,b:2,c:c,"
-		);
-	},
-	'test for if': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test',
-			"{% for user in users if user.active %}{{ user.name }},{% endfor %}" +
-		"");
-		//console.log(templateParser.getEvalCode('test').output);
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { users: [ { name: 'soywiz', active: true }, { name: 'test', active: false }, { name: 'demo', active: true } ] }),
-			"soywiz,demo,"
-		);
-	},
-	'test set tag': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test',
-			"{% autoescape false %}" +
-			"{% set foo1 = 'foo' %}" +
-			"{% set foo2 = [1, 2] %}" +
-			"{% set foo3 = {'foo': 'bar'} %}" +
-			"{% set foo4 = 'foo' ~ 'bar' %}" +
-			//"{% set foo5, bar = 'foo', 'bar' %}" +
-			"{{ foo1 }}," +
-			"{{ foo2 }}," +
-			"{{ foo3 }}," +
-			"{{ foo4 }}," +
-			"{% endautoescape %}" +
-		"");
-		//console.log(templateParser.getEvalCode('test').output);
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { v : 2 }),
-			'foo,[1,2],{"foo":"bar"},foobar,'
-		);
-	},
-	'test set tag tuple': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test',
-			"{% autoescape false %}" +
-			"{% set foo, bar = 'foo', 'bar' %}" +
-			"{{ foo }}," +
-			"{{ bear }}," +
-			"{% endautoescape %}" +
-		"");
-		//console.log(templateParser.getEvalCode('test').output);
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { v: 2 }),
-			'foo,bar,'
-		);
-	},
-	'test for loop key': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-
-		assert.fail('infinite loop [fixme]');
-
-		templateProvider.add('test',
-			"{% set n = 'a' %}" +
-			"{% for n in range(7, 9) %}" +
-			"{{ n }}," +
-			"{{ loop.index }}," +
-			"{{ loop.index0 }}", +
-			"{{ loop.revindex }}," +
-			"{{ loop.revindex0 }}," +
-			"{{ loop.first }}," +
-			"{{ loop.last }}," +
-			"{{ loop.length }}," +
-			"{{ loop.parent.n }}," +
-			"{% endfor %}" +
-		"");
-		//console.log(templateParser.getEvalCode('test').output);
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { v: 2 }),
-			"7,1,0,?,?,true,false,3,a," +
-			"8,2,1,?,?,false,false,3,a," +
-			"9,3,2,?,?,false,true,3,a," +
-		'');
-	},
-	'test range operator with filter': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test', '{% for n in "a"|upper.."e"|upper %}{{ n }}{% endfor %}');
-		//console.log(templateParser.getEvalCode('test').output);
-		assert.equal(
-			templateParser.compileAndRenderToString('test'),
-			"ABCDE"
-		);
-	},
-	'test simple for': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test', '{{ hi }}{{ n }}{% for n in [1, 2, 3, 4] %}{{ n }}{% endfor %}{{ n }}');
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { hi: 'Hello:' }),
-			"Hello:1234"
-		);
-	},
-	'test variable for': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test', '{{ hi }}{{ n }}{% for n in list %}{{ n * 2 }}{% endfor %}{{ n }}');
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { hi: 'Hello:', n : '[a]', list: [1, 2, 3, 4] }),
-			"Hello:[a]2468[a]"
-		);
-	},
-	'test for else': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test', '{% for n in list %}{{ n * 2 }}{% else %}empty{% endfor %}');
-		assert.equal(
-			templateParser.compileAndRenderToString('test', { hi: 'Hello:', n: '[a]', list: [] }),
-			"empty"
-		);
-	},
-	'test invalid endtag': function () {
-		var templateProvider = new MemoryTemplateProvider();
-		var templateParser = new TemplateParser(templateProvider);
-		templateProvider.add('test', '{% endautoescape %}');
-		try {
-			templateParser.compileAndRenderToString('test');
-			assert.fail();
-		} catch (e) {
-		}
-	},
-};

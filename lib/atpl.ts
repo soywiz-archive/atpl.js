5ac7d0d19e9d3140e805baceb4241b99bf378a4b
soywiz <soywiz@gmail.com>
2012-12-16 14:49:20 +0100

    - more work

--- lib/atpl.ts
+++ lib/atpl.ts
@@ -4,38 +4,78 @@ export import TemplateParser = module('./parser/TemplateParser');
 import TemplateProvider = module('./TemplateProvider');
 var FileSystemTemplateProvider = TemplateProvider.FileSystemTemplateProvider;
 
-var AdvancedTpl = function() {
-};
-
-//AdvancedTpl
-
-export function compileAndExecuteString(content) {
-	return content;
-}
-
-function templateTokenizer() {
-}
-
 var registryTemplateParser = {};
 
-export function compile(content: string, info: any) {
-	if (registryTemplateParser[info.root] === undefined) {
-		var templateProvider = new FileSystemTemplateProvider(info.root);
-		registryTemplateParser[info.root] = new TemplateParser.TemplateParser(templateProvider);
+export interface IOptions {
+	path?: string;
+	root?: string;
+	cache?: bool;
+}
+
+function internalCompile(options: IOptions) {
+	if (registryTemplateParser[options.root] === undefined) {
+		registryTemplateParser[options.root] = new TemplateParser.TemplateParser(
+			new FileSystemTemplateProvider(options.root, options.cache)
+		);
 	}
-	var templateParser = registryTemplateParser[info.root];
-	
-	//console.log("COMPILE:");
-	//console.log(info);
-	return function(info) {
-		var output = templateParser.compileAndRenderToString(info.filename.substr(info.root.length), info);
-		return output;
-		
-		/*
-		console.log("RENDER:");
-		console.log(info);
-		//return 'Hello World!';
-		return content;
-		*/
-	};
-};
+
+	return function(params: any) {
+		return registryTemplateParser[options.root].compileAndRenderToString(options.path.substr(options.root.length), params);
+	}
+}
+
+export function internalCompileString(content: string, options: IOptions) {
+	return function(params: any) {
+		throw (new Error("Not implemented internalCompileString"));
+		return "";
+	}
+}
+
+
+export function express2Compile(templateString: string, options: any): (params: any) => string {
+	return internalCompileString(templateString, options.settings['atpl options']);
+}
+
+function express3RenderFile(filename: string, options: any, callback: (err: Error, output: string) => void) {
+    // handle callback in options
+    if ('function' == typeof options) {
+        callback = options;
+        options = {};
+    }
+
+    options = options || {};
+
+    var params = {
+        path: filename,
+        base: options.settings['views'],
+        load: function(template) {
+            // render and return template
+            callback(null, template.render(options));
+        }
+    };
+
+    // mixin any options provided to the express app.
+    var view_options = options.settings['atpl options'];
+
+    try {
+		var func = internalCompile(params);
+    	var output = func({});
+    	callback(null, output);
+    } catch (e) {
+    	callback(e, '');
+    }
+}
+
+//Express 2x template engine compatibility required the following module export:
+//
+//exports.compile = function(templateString, options) {
+//  return a Function;
+//};
+export var compile = express2Compile;
+
+//Express 3x template engines should export the following:
+//
+//exports.__express = function(filename, options, callback) {
+//  callback(err, string);
+//};
+export var __express = express3RenderFile;

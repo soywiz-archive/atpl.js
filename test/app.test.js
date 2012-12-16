dfc74d68eff06172e164c438def6736f0a3247fa
soywiz <soywiz@gmail.com>
2012-12-16 19:21:39 +0100

    - Added test for extensions - Added init.bat

--- test/app.test.js
+++ test/app.test.js
@@ -3,26 +3,56 @@ var express = require("express");
 var supertest = require('supertest');
 var moment = require('moment');
 var async = require('async');
+var atpl = require('../lib/atpl');
 var app = express();
 
-app.engine('html', require('../lib/atpl').__express);
+app.engine('html', atpl.__express);
 app.set('devel', false);
 app.set('view engine', 'html');
 app.set('view cache', true);
 app.set('views', __dirname + '/../test/templates');
 
+var AtplExtension;
+AtplExtension = {
+	functions: (function () {
+		function functions() { }
+		functions.hashByDate = function hashByDate(count) {
+			var index = Math.round(new Date(2012, 0, 1).getTime() / (1000 * 3600 * 24));
+			index += 3;
+			return ((index) % (count)) + 1;
+		}
+		return functions;
+	})()
+};
+
+atpl.registerExtension(AtplExtension);
+
 app.get('/simple', function(req, res) {
 	res.render('simple', { name : 'Test' });
+});
+
+app.get('/extension', function (req, res) {
+	res.render('extension', {});
 });
 
 var test = supertest(app);
 
 describe('app', function() {
-	it('should work', function(done) {
+	it('simple should work', function(done) {
 		test
 			.get('/simple')
 			.end(function(err, res) {
 				assert.equal(res.text, 'Hello Test!');
+				done();
+			})
+		;
+	});
+
+	it('extension should work', function (done) {
+		test
+			.get('/extension')
+			.end(function (err, res) {
+				assert.equal(res.text, '4');
 				done();
 			})
 		;

3b3c46e1f8b5deba3c284d772ccdd93b7201a2af
soywiz <soywiz@gmail.com>
2013-03-03 14:44:37 +0100

    - Some work on #49 Refactorings, cleanups and documenting

--- lib/runtime/RuntimeUtils.ts
+++ lib/runtime/RuntimeUtils.ts
@@ -1,6 +1,4 @@
-﻿import DateFormat = module('./lib/DateFormat');
-import Format = module('./lib/Format');
-import util = module('util');
+﻿import util = module('util');
 
 export function normalizePath(path) {
 	var components = [];
@@ -291,17 +289,127 @@ export function random(min: number = 0, max: number = 2147483647): number {
 	return Math.round(Math.random() * (max - min)) + min;
 }
 
-export function sprintf(..._arguments: any[]) {
-	return Format.sprintf.apply(null, arguments);
-}
-
-export function date(format: string, date?: any, timezone?) {
-	if (date === undefined) date = new Date();
-	if (!(date instanceof Date)) {
-		// Number (unix timestamp?)
-		date = new Date(date * 1000);
+export var __sprintf = (function () {
+	function get_type(variable) {
+		return Object.prototype.toString.call(variable).slice(8, -1).toLowerCase();
 	}
-	return DateFormat.date(date, format);
+	function str_repeat(input, multiplier) {
+		for (var output = []; multiplier > 0; output[--multiplier] = input) {/* do nothing */ }
+		return output.join('');
+	}
+
+	var str_format = function () {
+		if (!str_format.cache.hasOwnProperty(arguments[0])) {
+			str_format.cache[arguments[0]] = str_format.parse(arguments[0]);
+		}
+		return str_format.format.call(null, str_format.cache[arguments[0]], arguments);
+	};
+
+	str_format.format = function (parse_tree, argv) {
+		var cursor = 1, tree_length = parse_tree.length, node_type = '', arg: any, output = [], i, k, match, pad, pad_character, pad_length;
+		for (i = 0; i < tree_length; i++) {
+			node_type = get_type(parse_tree[i]);
+			if (node_type === 'string') {
+				output.push(parse_tree[i]);
+			}
+			else if (node_type === 'array') {
+				match = parse_tree[i]; // convenience purposes only
+				if (match[2]) { // keyword argument
+					arg = argv[cursor];
+					for (k = 0; k < match[2].length; k++) {
+						if (!arg.hasOwnProperty(match[2][k])) {
+							throw (sprintf('[sprintf] property "%s" does not exist', match[2][k]));
+						}
+						arg = arg[match[2][k]];
+					}
+				}
+				else if (match[1]) { // positional argument (explicit)
+					arg = argv[match[1]];
+				}
+				else { // positional argument (implicit)
+					arg = argv[cursor++];
+				}
+
+				if (/[^s]/.test(match[8]) && (get_type(arg) != 'number')) {
+					throw (sprintf('[sprintf] expecting number but found %s', get_type(arg)));
+				}
+				switch (match[8]) {
+					case 'b': arg = arg.toString(2); break;
+					case 'c': arg = String.fromCharCode(arg); break;
+					case 'd': arg = parseInt(arg, 10); break;
+					case 'e': arg = match[7] ? arg.toExponential(match[7]) : arg.toExponential(); break;
+					case 'f': arg = match[7] ? parseInt(parseFloat(arg).toFixed(match[7])) : parseFloat(arg); break;
+					case 'o': arg = arg.toString(8); break;
+					case 's': arg = ((arg = String(arg)) && match[7] ? arg.substring(0, match[7]) : arg); break;
+					case 'u': arg = Math.abs(arg); break;
+					case 'x': arg = arg.toString(16); break;
+					case 'X': arg = arg.toString(16).toUpperCase(); break;
+				}
+				arg = (/[def]/.test(match[8]) && match[3] && arg >= 0 ? '+' + arg : arg);
+				pad_character = match[4] ? match[4] == '0' ? '0' : match[4].charAt(1) : ' ';
+				pad_length = match[6] - String(arg).length;
+				pad = match[6] ? str_repeat(pad_character, pad_length) : '';
+				output.push(match[5] ? arg + pad : pad + arg);
+			}
+		}
+		return output.join('');
+	};
+
+	str_format.cache = {};
+
+	str_format.parse = function (fmt) {
+		var _fmt = fmt, match: any = [], parse_tree = [], arg_names = 0, field_match: any;
+		while (_fmt) {
+			if ((match = /^[^\x25]+/.exec(_fmt)) !== null) {
+				parse_tree.push(match[0]);
+			}
+			else if ((match = /^\x25{2}/.exec(_fmt)) !== null) {
+				parse_tree.push('%');
+			}
+			else if ((match = /^\x25(?:([1-9]\d*)\$|\(([^\)]+)\))?(\+)?(0|'[^$])?(-)?(\d+)?(?:\.(\d+))?([b-fosuxX])/.exec(_fmt)) !== null) {
+				if (match[2]) {
+					arg_names |= 1;
+					var field_list = [], replacement_field = match[2], field_match = [];
+					if ((field_match = /^([a-z_][a-z_\d]*)/i.exec(replacement_field)) !== null) {
+						field_list.push(field_match[1]);
+						while ((replacement_field = replacement_field.substring(field_match[0].length)) !== '') {
+							if ((field_match = /^\.([a-z_][a-z_\d]*)/i.exec(replacement_field)) !== null) {
+								field_list.push(field_match[1]);
+							}
+							else if ((field_match = /^\[(\d+)\]/.exec(replacement_field)) !== null) {
+								field_list.push(field_match[1]);
+							}
+							else {
+								throw ('[sprintf] huh?');
+							}
+						}
+					}
+					else {
+						throw ('[sprintf] huh?');
+					}
+					match[2] = field_list;
+				}
+				else {
+					arg_names |= 2;
+				}
+				if (arg_names === 3) {
+					throw ('[sprintf] mixing positional and named placeholders is not (yet) supported');
+				}
+				parse_tree.push(match);
+			}
+			else {
+				throw ('[sprintf] huh?');
+			}
+			_fmt = _fmt.substring(match[0].length);
+		}
+		return parse_tree;
+	};
+
+	return str_format;
+})();
+
+export function sprintf(..._arguments: any[]) {
+	return __sprintf.apply(null, arguments);
 }
 
 export function defined(value: any) {
@@ -419,4 +527,78 @@ export function escapeCssString(text: string) {
 // rawurlencode
 export function escapeUrlString(str: string) {
 	return encodeURIComponent(String(str)).replace(/[!'\(\)\*]/g, (match) => { return '%' + (('00' + match.charCodeAt(0).toString(16)).substr(-2)); });
+}
+
+export function getOrdinalFor(intNum) {
+	return (((intNum = Math.abs(intNum) % 100) % 10 == 1 && intNum != 11) ? "st"
+            : (intNum % 10 == 2 && intNum != 12) ? "nd" : (intNum % 10 == 3
+            && intNum != 13) ? "rd" : "th");
+}
+export function getISO8601Year(aDate) {
+	var d = new Date(aDate.getFullYear() + 1, 0, 4);
+	if ((d - aDate) / 86400000 < 7 && (aDate.getDay() + 6) % 7 < (d.getDay() + 6) % 7)
+		return d.getFullYear();
+	if (aDate.getMonth() > 0 || aDate.getDate() >= 4)
+		return aDate.getFullYear();
+	return aDate.getFullYear() - (((aDate.getDay() + 6) % 7 - aDate.getDate() > 2) ? 1 : 0);
+}
+export function getISO8601Week(aDate) {
+	// Get a day during the first week of the year.
+	var d = new Date(getISO8601Year(aDate), 0, 4);
+	// Get the first monday of the year.
+	d.setDate(d.getDate() - (d.getDay() + 6) % 7);
+	return Math.floor((aDate - d) / 604800000) + 1;
+}
+
+var date_shortDays = "Sun,Mon,Tue,Wed,Thu,Fri,Sat".split(",");
+var date_fullDays = "Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday".split(",");
+var date_shortMonths = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec".split(",");
+var date_fullMonths = "January,February,March,April,May,June,July,August,September,October,November,December".split(",");
+
+export function date(format, date, timezone?) {
+	if (date === undefined) date = new Date();
+	if (!(date instanceof Date)) {
+		// Number (unix timestamp?)
+		date = new Date(date * 1000);
+	}
+
+	if (typeof format !== "string" || /^\s*$/.test(format))
+		return date + "";
+	var jan1st = new Date(date.getFullYear(), 0, 1);
+	var me = date;
+	return format.replace(/[dDjlNSwzWFmMntLoYyaABgGhHisu]/g, function (option) {
+		switch (option) {
+			case "d": return ("0" + me.getDate()).replace(/^.+(..)$/, "$1"); // Day of the month, 2 digits with leading zeros
+			case "D": return date_shortDays[me.getDay()]; // A textual representation of a day, three letters
+			case "j": return me.getDate(); // Day of the month without leading zeros
+			case "l": return date_fullDays[me.getDay()]; // A full textual representation of the day of the week
+			case "N": return (me.getDay() + 6) % 7 + 1; // ISO-8601 numeric representation of the day of the week
+			case "S": return getOrdinalFor(me.getDate()); // English ordinal suffix for the day of the month, 2 characters
+			case "w": return me.getDay(); // Numeric representation of the day of the week
+			case "z": return Math.ceil((jan1st - me) / 86400000); // The day of the year (starting from 0)
+			case "W": return ("0" + getISO8601Week(me)).replace(/^.(..)$/, "$1"); // ISO-8601 week number of year, weeks starting on Monday
+			case "F": return date_fullMonths[me.getMonth()]; // A full textual representation of a month, such as January or March
+			case "m": return ("0" + (me.getMonth() + 1)).replace(/^.+(..)$/, "$1"); // Numeric representation of a month, with leading zeros
+			case "M": return date_shortMonths[me.getMonth()]; // A short textual representation of a month, three letters
+			case "n": return me.getMonth() + 1; // Numeric representation of a month, without leading zeros
+			case "t": return new Date(me.getFullYear(), me.getMonth() + 1, -1).getDate(); // Number of days in the given month
+			case "L": return new Date(me.getFullYear(), 1, 29).getDate() == 29 ? 1 : 0;// Whether it's a leap year
+			// ISO-8601 year number. This has the same value as Y, except that if the
+			// ISO week number (W) belongs to the previous or next year, that year is
+			// used instead.
+			case "o": return getISO8601Year(me);
+			case "Y": return me.getFullYear(); // A full numeric representation of a year, 4 digits
+			case "y": return (me.getFullYear() + "").replace(/^.+(..)$/, "$1"); // A two digit representation of a year
+			case "a": return me.getHours() < 12 ? "am" : "pm"; // Lowercase Ante meridiem and Post meridiem
+			case "A": return me.getHours() < 12 ? "AM" : "PM"; // Uppercase Ante meridiem and Post meridiem
+			case "B": return Math.floor((((me.getUTCHours() + 1) % 24) + me.getUTCMinutes() / 60 + me.getUTCSeconds() / 3600) * 1000 / 24); // Swatch Internet time
+			case "g": return me.getHours() % 12 != 0 ? me.getHours() % 12 : 12; // 12-hour format of an hour without leading zeros
+			case "G": return me.getHours(); // 24-hour format of an hour without leading zeros
+			case "h": return ("0" + (me.getHours() % 12 != 0 ? me.getHours() % 12 : 12)).replace(/^.+(..)$/, "$1"); // 12-hour format of an hour with leading zeros
+			case "H": return ("0" + me.getHours()).replace(/^.+(..)$/, "$1"); // 24-hour format of an hour with leading zeros
+			case "i": return ("0" + me.getMinutes()).replace(/^.+(..)$/, "$1"); // Minutes with leading zeros
+			case "s": return ("0" + me.getSeconds()).replace(/^.+(..)$/, "$1"); // Seconds, with leading zeros
+			case "u": return me.getMilliseconds(); // Milliseconds
+		}
+	});
 }
--- lib/runtime/Scope.ts
+++ lib/runtime/Scope.ts
@@ -2,6 +2,14 @@
 	constructor(public scope: any = {}) {
 	}
 
+	/**
+	 * Creates a new scope temporarlly (while executing the callback).
+	 * Writting variables will set temporarily in the new scope, while
+	 * reading will try to get the value from the new scope and then
+	 * in following scopes.
+	 *
+	 * @param inner Callback where the new scope will be available.
+	 */
 	createScope(inner: () => void) {
 		var newScope = {};
 		var oldScope = this.scope;
@@ -14,31 +22,42 @@
 		}
 	}
 
+	/**
+	 * Obtains the parent scope.
+	 */
 	getParent() {
 		return this.scope['__proto__'];
 	}
 
-	get(key) {
+	/**
+	 * Gets a value in the most recent available scope.
+	 */
+	get(key: string): any {
 		return this.scope[key];
 	}
 
+	/**
+	 * Gets all the scope values. (slow)
+	 */
 	getAll() {
 		var object = {};
-		var parentScope = this.scope['__proto__'];
-		if (parentScope instanceof Scope) {
-			object = parentScope.getAll();
-		}
-		for (var key in this.scope) {
-			object[key] = this.scope[key];
-		}
+		var parentScope = this.getParent();
+		if (parentScope instanceof Scope) object = parentScope.getAll();
+		for (var key in this.scope) object[key] = this.scope[key];
 		return object;
 	}
 
-	set(key, value) {
+	/**
+	 * Sets a value in the scope
+	 */
+	set(key: string, value: any): any {
 		return this.scope[key] = value;
 	}
 
-	setAll(object) {
+	/**
+	 * Sets a list of values in the current scope.
+	 */
+	setAll(object: any): void {
 		for (var key in object) this.set(key, object[key]);
 	}
 }
